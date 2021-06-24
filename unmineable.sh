6493c2db4f1149f08c9d3a710217243ad226a779
palram5642 <86397661+palram5642@users.noreply.github.com>
2021-06-24 11:20:48 +0530

    Create unmineable.sh

---
+++ unmineable.sh
@@ -0,0 +1,6 @@
+# unmineable
+sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux-x64.tar.gz
+tar -xf xmrig-6.12.1-linux-x64.tar.gz
+cd xmrig-6.12.1
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u BTC:33mCCoZBspfeER42ZBe5tJfjrYufimf7pG.unmineable_miner_worker1+100000
